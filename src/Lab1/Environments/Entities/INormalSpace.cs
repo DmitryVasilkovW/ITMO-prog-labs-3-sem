2aaaee79ee65d0a234657dd0044d28f5e708acc6
Dmitry Vasilkov <dmitryvasilkovw@gmail.com>
2023-10-09 21:49:15 +0300

    feat: new exception and new interfaces for checks

---
+++ src/Lab1/Environments/Entities/INormalSpace.cs
@@ -0,0 +1,8 @@
+using Itmo.ObjectOrientedProgramming.Lab1.Spaceship.Entities;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Environments.Entities;
+
+public interface INormalSpace
+{
+    ISpaceship Damage(ISpaceship ship, AdditionalSafetyDevices device);
+}
---
+++ src/Lab1/Environments/Entities/StatusOfShips.cs
@@ -0,0 +1,10 @@
+namespace Itmo.ObjectOrientedProgramming.Lab1.Environments.Entities;
+
+public enum StatusOfShips
+{
+    None,
+    Success,
+    LossOfShip,
+    DestructionOfTheShip,
+    CrewDeaths,
+}
