112793dcaebcd6be69c1644909a95e855b18c162
Dmitry Vasilkov <dmitryvasilkovw@gmail.com>
2023-10-18 20:48:12 +0300

    refactor: bilder and entities

--- src/Lab2/Computer/Models/CPU/Models/ICPU.cs
+++ src/Lab2/Computer/Models/CPU/Models/ICPU.cs
@@ -1,12 +1,15 @@
-namespace Itmo.ObjectOrientedProgramming.Lab2.Computer.Models.CPU.Services;
+using Itmo.ObjectOrientedProgramming.Lab2.Computer.Models.Socket;
+
+namespace Itmo.ObjectOrientedProgramming.Lab2.Computer.Models.CPU.Models;
 
 public interface ICPU
 {
-    string? Name { get; }
+    string? Name { get; init; }
     bool AvailabilityOfABuiltInVideoCore { get; }
     int SupportedMemoryFrequencies { get; }
     int HeatEmission { get; }
     int ConsumptionPower { get; }
     int CoreFrequency { get; }
     int NumberOfCores { get; }
+    ISocket? Socket { get; init; }
 }
--- src/Lab2/Computer/Services/Databases/TableOfCPUCharacteristics.cs
+++ src/Lab2/Computer/Services/Databases/TableOfCPUCharacteristics.cs
@@ -1,8 +1,9 @@
-using Itmo.ObjectOrientedProgramming.Lab2.Computer.Models.CPU.Services;
+using Itmo.ObjectOrientedProgramming.Lab2.Computer.Models.CPU.Models;
+using Itmo.ObjectOrientedProgramming.Lab2.Computer.Models.Socket;
 
-namespace Itmo.ObjectOrientedProgramming.Lab2.Computer.Models.CPU.Entities;
+namespace Itmo.ObjectOrientedProgramming.Lab2.Computer.Services.Databases;
 
-public class AM4 : ICPU
+public class TableOfCPUCharacteristics : IPartCharacteristics, ICPU
 {
     public string? Name { get; init; }
     public bool AvailabilityOfABuiltInVideoCore { get; init; }
@@ -11,4 +12,5 @@ public class AM4 : ICPU
     public int ConsumptionPower { get; init; }
     public int CoreFrequency { get; init; }
     public int NumberOfCores { get; init; }
+    public ISocket? Socket { get; init; }
 }
