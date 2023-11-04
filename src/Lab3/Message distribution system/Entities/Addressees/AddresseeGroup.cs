7f199eee2b48d4fb5f1ba3ed2b6d6d6cd1bce10f
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-11-04 13:59:55 +0300

    fix: all

--- src/Lab3/Message distribution system/Entities/Addressees/AddresseeGroup.cs
+++ src/Lab3/Message distribution system/Entities/Addressees/AddresseeGroup.cs
@@ -1,19 +1,16 @@
 using System.Collections.Generic;
 using Itmo.ObjectOrientedProgramming.Lab3.Messagedistributionsystem.Models.Messages;
-using Itmo.ObjectOrientedProgramming.Lab3.Messagedistributionsystem.Services;
 
 namespace Itmo.ObjectOrientedProgramming.Lab3.Messagedistributionsystem.Entities.Addressees;
 
 public class AddresseeGroup : IAddressee
 {
     private IList<IAddressee> _addressees;
-    private IConsoleLogger _logger;
 
-    public AddresseeGroup(string name, IConsoleLogger logger)
+    public AddresseeGroup(string name)
     {
         Name = name;
         _addressees = new List<IAddressee>();
-        _logger = logger;
     }
 
     public string Name { get; }
@@ -23,7 +20,6 @@ public class AddresseeGroup : IAddressee
         foreach (IAddressee addressee in _addressees)
         {
             addressee.GetMessage(message);
-            _logger.Log(message, addressee);
         }
     }
 }
