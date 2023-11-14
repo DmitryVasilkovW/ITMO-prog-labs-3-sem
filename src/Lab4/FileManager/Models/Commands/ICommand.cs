4f5618dabdb4fdab9af14ca8fd5d004e1f549f90
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-11-14 12:02:53 +0300

    feat: Commands

---
+++ src/Lab4/FileManager/Models/Commands/ICommand.cs
@@ -0,0 +1,7 @@
+using Itmo.ObjectOrientedProgramming.Lab4.FileManager.Services.ResponsibilityChain;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.FileManager.Models.Commands;
+
+public interface ICommand : IChainLink
+{
+}
