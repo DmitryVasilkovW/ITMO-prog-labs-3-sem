384a097b6a61a6bb879809286ea82fb62c0a5324
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-11-15 14:13:22 +0300

    feat: tree commands

---
+++ src/Lab4/FileManager/Models/Commands/TreeCommands/TreeGotoCommand.cs
@@ -0,0 +1,9 @@
+namespace Itmo.ObjectOrientedProgramming.Lab4.FileManager.Models.Commands.TreeCommands;
+
+public class TreeGotoCommand : ICommand
+{
+    public void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ src/Lab4/FileManager/Models/Commands/TreeCommands/TreeListCommand.cs
@@ -0,0 +1,9 @@
+namespace Itmo.ObjectOrientedProgramming.Lab4.FileManager.Models.Commands.TreeCommands;
+
+public class TreeListCommand : ICommand
+{
+    public void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+}
