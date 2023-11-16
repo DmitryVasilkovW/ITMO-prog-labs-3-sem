0eabaa105843202a553aa072f7f43b38fe2dd1dd
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-11-16 21:50:13 +0300

    refactor: command

--- src/Lab4/FileManager/Models/Commands/ConnectionRelatedCommands/ConnectCommand.cs
+++ src/Lab4/FileManager/Models/Commands/ConnectionRelatedCommands/ConnectCommand.cs
@@ -2,6 +2,13 @@ namespace Itmo.ObjectOrientedProgramming.Lab4.FileManager.Models.Commands;
 
 public class ConnectCommand : ICommand
 {
+    private string _fullpath;
+
+    public ConnectCommand(string fullpath)
+    {
+        _fullpath = fullpath;
+    }
+
     public void Execute()
     {
     }
