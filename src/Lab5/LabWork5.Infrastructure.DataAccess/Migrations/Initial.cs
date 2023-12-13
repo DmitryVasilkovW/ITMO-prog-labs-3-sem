1d173f0dae6d614637531ed2e63a788803cc6187
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-12-13 21:08:50 +0300

    fix: initial

--- src/Lab5/LabWork5.Infrastructure.DataAccess/Migrations/Initial.cs
+++ src/Lab5/LabWork5.Infrastructure.DataAccess/Migrations/Initial.cs
@@ -8,40 +8,32 @@ public class Initial : SqlMigration
 {
     protected override string GetUpSql(IServiceProvider serviceProvider) =>
         """
-    create type user_role as enum
-    (
-        'admin',
-        'user',
-    );
-
-    create type withdrawal_result_state as enum
-    (
-        'success',
-        'Insufficient funds',
-    );
-
     create table users
     (
-        user_id bigint primary key generated always as identity ,
-        user_name text not null ,
-        user_role user_role not null 
+        user_id bigint primary key generated always as identity,
+        user_name text not null,
+        user_balance bigint not null,
+        user_passwords bigint not null,
+    );
+
+    create table history
+    (
+        user_id bigint not null,
+        operation text not null,
+    );
+
+    create table admin
+    (
+        abmin_id bigint primary key generated always as identity,
+        user_name text not null,
+        admin_passwords bigint not null,
     );
     """;
 
     protected override string GetDownSql(IServiceProvider serviceProvider) =>
         """
     drop table users;
-    drop table shops;
-    drop table product_categories;
-    drop table products;
-    drop table shop_products;
-    drop table orders; 
-    drop table order_products;
-    drop table order_results;
-    drop table order_result_products;
-
-    drop type user_role;
-    drop type order_state;
-    drop type order_result_state;
+    drop table history;
+    drop table admin;
     """;
 }
